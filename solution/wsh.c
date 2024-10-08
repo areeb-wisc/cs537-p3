cb803ccaef8e40ba8ad0a7a48ab1beb9ed5abdf0
Mohammad Areeb <areeb@rockhopper-09.cs.wisc.edu>
2024-10-08 09:20:14 -0500

    added wsh_history()

--- solution/wsh.c
+++ solution/wsh.c
@@ -130,6 +130,113 @@ void print_vars() {
 
 /********************************** DICTIONARY END *******************************/
 
+/***************************** HISTORY START **********************************/
+
+typedef struct circular_queue {
+    char** words;
+    int n, r, f;
+} cqueue;
+
+void init(cqueue* cq, int size) {
+    printf("init\n");
+    cq->n = size;
+    cq->r = -1;
+    cq->f = -1;
+    cq->words = (char**)malloc(size * sizeof(char*));
+}
+
+cqueue* history;
+
+void push(cqueue* cq, const char* word) {
+    printf("push %s\n", word);
+    if (cq->r == -1 || cq->r == (cq->f + 1) % cq->n)
+        cq->r = (cq->r + 1) % cq->n;
+    cq->f = (cq->f + 1) % cq->n;
+    cq->words[cq->f] = clone_str(word);
+}
+
+void pop(cqueue* cq) {
+    printf("pop\n");
+    if (cq->f == cq->r) {
+        cq->f = -1;
+        cq->r = -1;
+    } else
+        cq->r = (cq->r + 1) % cq->n;
+}
+
+int getsize(cqueue* cq) {
+    if (cq->f == -1)
+        return 0;
+    if (cq->f >= cq->r)
+        return cq->f - cq->r + 1;
+    return cq->f + 1 + cq->n - cq->r;
+}
+
+char* get(cqueue* cq, int no) {
+    if (no < 1 || no > cq->n)
+        return NULL;
+    int i = (cq->f - (no - 1) + cq->n) % cq->n;
+    bool is_valid = false;
+    if (cq->f == cq->r)
+        is_valid = (i == cq->f);
+    else if (cq->r < cq->f)
+        is_valid = (cq->r <= i && i <= cq->f);
+    else
+        is_valid = (0 <= i && i <= cq->f) || (cq->r <= i && i < cq->n);
+    if (is_valid)
+        return clone_str(cq->words[i]);
+    return NULL;
+}
+
+void print(cqueue* cq, int no) {
+    char* word = get(cq, no);
+    if (word == NULL)
+        printf("NULL\n");
+    else
+        printf("%s\n", word);
+}
+
+void display(cqueue* cq) {
+    printf("n = %d, r = %d, f = %d\n", cq->n, cq->r, cq->f);
+    printf("history->words:\n");
+    int i = cq->r, size = getsize(cq);
+    while (size--) {
+        printf("%s\n", cq->words[i]);
+        i = (i + 1) % cq->n;
+    }
+    printf("\n");
+}
+
+void resize(cqueue** cq, int size) {
+
+    printf("resize to %d\n", size);
+    if (size == (*cq)->n)
+        return;
+    
+    if (size < (*cq)->n) {
+        int drop = (*cq)->n - size;
+        while(drop--)
+            pop(*cq);
+    }
+    
+    cqueue* newcq = (cqueue*)malloc(sizeof(cqueue));
+    init(newcq, size);
+
+    int i = (*cq)->r, oldsize = getsize(*cq);
+    while (oldsize--) {
+        push(newcq, (*cq)->words[i]);
+        i = (i + 1) % (*cq)->n;
+    }
+    
+    display(newcq);
+    free(*cq);
+    *cq = newcq;
+
+}
+
+/**************************** HISTORY END ************************************/
+
+
 /******************** VARNAME DEREFERENCING HELPERS START *******************/
 
 char* dereference(const char* ovarname) {
@@ -447,6 +554,7 @@ void wsh_export(const char* otoken) {
 
 void wsh_history() {
     printf("wsh_history() called\n");
+    display(history);
 }
 
 void wsh_local(const char* otoken) {
@@ -488,6 +596,11 @@ void wsh_vars() {
 
 /*************************** BUILT-IN CALLS END ****************************/
 
+void init_history(int size) {
+    history = (cqueue*)malloc(sizeof(cqueue));
+    init(history, size);
+}
+
 void init_env_vars() {
     last_exit_code = putenv("PATH=/bin");
 }
@@ -525,6 +638,8 @@ int main(int argc, char* argv[]) {
         wsh_prompt = "";
     }
 
+    init_history(5);
+    display(history);
     init_env_vars();
     init_shell_vars();
     // print_vars();
@@ -563,6 +678,8 @@ int main(int argc, char* argv[]) {
         // Step 3: record history for non built-in commands
         if (!is_builtin(command)) {
             // record history
+            push(history, line);
+            display(history);
         }
 
         // Step 4: perform I/O redirection if any
